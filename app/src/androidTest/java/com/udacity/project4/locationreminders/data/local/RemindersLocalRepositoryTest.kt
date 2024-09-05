2c3bb5afa5d5f6ec55aab368b1e18d09f042ed4d
tnanhd <tnanh.dev@gmail.com>
2024-09-05 10:01:22 +0700

    Feat: Add android test for RemindersLocalRepository

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -9,15 +9,18 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.`is`
+import kotlinx.coroutines.test.runTest
 import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.UUID
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -25,6 +28,78 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var remindersLocalRepository: RemindersLocalRepository
+    private lateinit var remindersDatabase: RemindersDatabase
+
+    @Before
+    fun initDb() {
+        remindersDatabase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        remindersLocalRepository = RemindersLocalRepository(
+            remindersDatabase.reminderDao(),
+            Dispatchers.Main
+        )
+    }
+
+    @After
+    fun closeDb() = remindersDatabase.close()
+
+    @Test
+    fun insertReminder_thenLoadReminders_shouldReturnNonEmptyList() = runTest {
+        val id = UUID.randomUUID().toString()
+        val title = "title"
+        val description = "description"
+        val location = "location"
+        val latitude = 0.0
+        val longitude = 0.0
+
+        val reminder = ReminderDTO(title, description, location, latitude, longitude, id)
+        remindersLocalRepository.saveReminder(reminder)
+
+        val reminders = remindersLocalRepository.getReminders()
+
+        assertThat(reminders, notNullValue())
+        assertThat(reminders is Result.Success, `is`(true))
+        assertThat((reminders as Result.Success).data, `is`(not(emptyList())))
+        reminders.data[0].let {
+            assertThat(it.id, `is`(id))
+            assertThat(it.title, `is`(title))
+            assertThat(it.description, `is`(description))
+            assertThat(it.location, `is`(location))
+            assertThat(it.latitude, `is`(latitude))
+            assertThat(it.longitude, `is`(longitude))
+        }
+
+        val remindersById = remindersLocalRepository.getReminder(id)
+        assertThat(remindersById, notNullValue())
+        assertThat(remindersById is Result.Success, `is`(true))
+        assertThat((remindersById as Result.Success).data, `is`(instanceOf(ReminderDTO::class.java)))
+        remindersById.data.let {
+            assertThat(it.id, `is`(id))
+            assertThat(it.title, `is`(title))
+            assertThat(it.description, `is`(description))
+            assertThat(it.location, `is`(location))
+            assertThat(it.latitude, `is`(latitude))
+            assertThat(it.longitude, `is`(longitude))
+        }
+
+        remindersLocalRepository.deleteAllReminders()
+        val emptyReminders = remindersLocalRepository.getReminders()
+        assertThat(emptyReminders is Result.Success, `is`(true))
+        assertThat((emptyReminders as Result.Success).data, `is`(emptyList()))
+    }
+
+    @Test
+    fun findNonExistingReminder_shouldReturnEmptyList() = runTest {
+        val reminder = remindersLocalRepository.getReminder("non-existing-id")
+        assertThat(reminder is Result.Error, `is`(true))
+        assertThat((reminder as Result.Error).message, `is`("Reminder not found!"))
+    }
 
 }
